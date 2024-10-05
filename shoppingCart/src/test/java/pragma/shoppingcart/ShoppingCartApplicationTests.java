3f880b19899eb4337838f4078e951083a096649d
Camilo Tello <camilotello04@gmail.com>
2024-10-05 11:58:56 -0500

    creacion proyecto shopping cart

---
+++ shoppingCart/src/test/java/pragma/shoppingcart/ShoppingCartApplicationTests.java
@@ -0,0 +1,13 @@
+package pragma.shoppingcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
